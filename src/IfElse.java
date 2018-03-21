172d8a182a03ba843556c20dda3c353277ef902f
sergpot1 <serg.pot@bk.ru>
2018-03-21 22:45:28 +0300

    2103lesson

---
+++ src/IfElse.java
@@ -0,0 +1,101 @@
+/*
+public class IfElse {
+    public static void main(String[] args){
+        System.out.println("Ветвление");
+        boolean condition = true;
+        // проверка условия, если оно true то выполняется код ниже
+        if (condition) {
+            System.out.println("condition = true");
+        }
+        int a =4;
+        int b= 6;
+        if (a < b) {
+            System.out.println("a < b");
+        }
+
+        // &&, ||, ^ (XOR), !=, &, |
+
+        // switch
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Pon");
+                break;
+            case 2:
+                System.out.println("Vt");
+                break;
+            case 3:
+                System.out.println("Sr");
+                break;
+            case 4:
+                System.out.println("CHt");
+                break;
+            case 5:
+                System.out.println("Pyat");
+                break;
+            case 6:
+                System.out.println("Sub");
+                break;
+            case 7:
+                System.out.println("Vosk");
+                break;
+            default:
+                System.out.println("net takogo dnya");
+        }
+        int num = 13;
+        String type;
+
+        if (num % 2 == 0) {
+            type = "even";
+        } else {
+            type = "odd";
+            System.out.println(type);
+
+
+            // тернарный оператор
+            type = num % 2 == 0 ? "even" : "odd";
+
+            //condition ? "true" : "false";
+
+            // while
+            // for
+            int i;
+            for (i=1; i<10 ; i+=2) {
+                if (i % 2 != 0)
+                    System.out.println(i);
+
+            }
+
+            // do while
+
+            */
+/*do {
+                System.out.println("do while")
+            } while (condition); *//*
+
+
+            // ot 5 do 1
+            int j = 5;
+            do {
+                System.out.println("j" + j);
+                j--;
+            } while (j >= 1);
+
+
+            */
+/*for (int i =0; i <10; i++) {
+                // code
+                for (int j =0; j <10; j++)
+                    // code
+                    if (yslovie)
+                        break; // break outer*//*
+
+            }
+
+
+
+
+        }
+    }
+}
+*/
---
+++ src/zadacha.java
@@ -0,0 +1,12 @@
+public class zadacha {
+    public static void main (String[] args){
+
+        // int n = 54;
+        // System.out.println(n / 10 + n % 10);
+
+        int m = 754;
+        System.out.println(m % 100 / 10 + m % 100 % 10 + m/100);
+
+
+    }
+}
