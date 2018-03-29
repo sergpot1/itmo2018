e9abaf3d8b98cff6e102b2e8830f0fb17c72d6ad
sergpot1 <serg.pot@bk.ru>
2018-03-29 14:12:23 +0300

    task2

---
+++ src/dz30.03/task2Figure/Rectangle.java
@@ -0,0 +1,79 @@
+package task2Figure;
+
+public class Rectangle {
+    private double x1, x2, x3;
+    private double y1, y2, y3;
+    private double a, b, c;
+
+
+
+    public Rectangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+
+        this.a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        this.b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+        this.a = Math.abs(a);
+        this.b = Math.abs(b);
+
+    }
+
+    public double Perimetr() {
+        return (double) (2 * (a + b));
+    }
+
+    public double Square() {
+        return (double) (a * b);
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+    public void setX3(double x3) {
+        this.x3 = x3;
+    }
+
+    public void setY3(double y3) {
+        this.y3 = y3;
+    }
+
+
+
+
+    public double getY1() {
+        return y1;
+    }
+    public double getX1() {
+        return x1;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+    public double getX2() {
+        return x2;
+    }
+
+    public double getY3() {
+        return y3;
+    }
+    public double getX3() {
+        return x3;
+    }
+}
---
+++ src/dz30.03/task2Figure/figure.java
@@ -0,0 +1,19 @@
+package task2Figure;
+
+class figure {
+    public static void main(String[] args){
+        Circle circle = new Circle(12, 4.5, 3, 6);
+        System.out.println("\n");
+        System.out.println("Circumference: " + circle.Perimetr() + " Square: " + circle.Square() + " Coordinates: x1 = " + circle.getX1() + ", y1 = " + circle.getY1() + "; " +
+                " x2 = " + circle.getX2() + ", y2 = " + circle.getY2() + ";");
+        Rectangle rectangle = new Rectangle(10, 5, 6, 5, 8, 9);
+        System.out.println("Perimeter: " + rectangle.Perimetr() + " Square: " + rectangle.Square() + " Coordinates: x1 = " + rectangle.getX1() + ", y1 = " + rectangle.getY1() + "; " +
+                " x2 = " + rectangle.getX2() + ", y2 = " + rectangle.getY2() + "; " + "x3 = " + rectangle.getX3() + ", y3 = " + rectangle.getY3() + ";");
+        Triangle triangle = new Triangle(4, 5, 7, 8, 3, 5);
+        System.out.println("Perimeter: " + triangle.Perimetr() + " Square: " + triangle.Square() + " Coordinates: x1 = " + triangle.getX1() + ", y1 = " + triangle.getY1() + "; " +
+                " x2 = " + triangle.getX2() + ", y2 = " + triangle.getY2() + "; " + "x3 = " + triangle.getX3() + ", y3 = " + triangle.getY3() + ";");
+    }
+}
+
+
+
